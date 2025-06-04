7e00ba0229951d2b02fe7e4cb9faea2a413f2258
Ashutosh Choudhary <ashutoshchoudhary.job@gmail.com>
2025-06-05 00:30:44 +0530

    fixed broken markers on map

--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -9,6 +9,14 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+});
+
 import api from '../services/api';
 
 interface Charger {
